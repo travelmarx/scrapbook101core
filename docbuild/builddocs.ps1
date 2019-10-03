d529cdbc1ba89c6111934324920f49442e57407f
Travelmarx <travelmarx@live.com>
2019-10-03 22:54:17 +0200

    Add docfx into build script.

--- docbuild/builddocs.ps1
+++ docbuild/builddocs.ps1
@@ -1,4 +1,31 @@
-write-host "## Start of build script."
-choco install docfx -y
-docfx --help
-write-host "## Location is" $PSScriptRoot.ToString()
+Write-Host -ForegroundColor Green "## Start of build script."
+Write-Host -ForegroundColor Green "## Location is" $PSScriptRoot.ToString()
+try {
+    Write-Host -ForegroundColor Green "## Install chocolatey."
+    choco install docfx -y
+
+    Write-Host -ForegroundColor Green "## Run DocFx."
+    docfx metadata
+    docfx build
+    Write-Host -ForegroundColor Green "## Docfx ran successfully."
+
+    Write-Host -ForegroundColor Green "## Copy files."
+    Get-ChildItem .\docs -Recurse | Remove-Item -Recurse
+
+    $files = Get-ChildItem -Path .\docbuild\_site
+    foreach ($f in $files)
+    {
+        Copy-Item $f.FullName -Destination .\docs -Recurse -Force
+    }
+    
+    $directoryInfo = Get-ChildItem .\docs | Measure-Object
+    if ($directoryInfo.Count -ne 0)
+    {
+        Write-Host "There is content to add to repo."
+    }
+    
+    Write-Host -ForegroundColor Green "## Build ran successfully."
+}
+finally {
+    Write-Host -ForegroundColor Red "Something went wrong in the build. Check the output."
+}
