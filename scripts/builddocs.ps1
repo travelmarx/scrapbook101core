bfef76714e1458b49f56bbcae5bd90b6c597c981
Travelmarx <travelmarx@live.com>
2019-10-05 23:06:30 +0200

    Debug script.

--- scripts/builddocs.ps1
+++ scripts/builddocs.ps1
@@ -20,8 +20,8 @@ try {
     Get-ChildItem .\docs -Recurse | Remove-Item -Recurse
 
     Write-Host "## Copy files. Second step: start copy."
-    Write-Host "## pwd:" (pwd).Path    
     $files = Get-ChildItem -Path .\docbuild\_site
+    Write-Host "## Files count =" $files.ToString()
     foreach ($f in $files)
     {
         Wrte-Host "## ## Copying" $f.FullName
